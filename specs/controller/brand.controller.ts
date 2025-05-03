990a096a08f67ee2502bb829aaf385d4447b4665
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2025-05-03 11:14:20 +0300

    add controller

---
+++ specs/controller/brand.controller.ts
@@ -0,0 +1,33 @@
+import * as supertest from 'supertest';
+const request = supertest('https://practice-react.sdetunicorns.com/api/test');
+
+type Brand = {
+    [key: string]: string;
+};
+
+class BrandController {
+    getBrands() {
+        return request.get('/brands');
+    }
+
+    getBrandById(brandId: string) {
+        return request.get(`/brands/${brandId}`);
+    }
+
+    postBrands(payload: Brand) {
+        return request.post('/brands').send(payload);
+    }
+
+    putBrands(brandId: string, payload: Brand) {
+        return request.put(`/brands/${brandId}`).send(payload);
+    }
+
+    deleteBrands(brandId: string) {
+        return request.delete(`/brands/${brandId}`);
+    }   
+
+}
+
+export default new BrandController();
+
+
