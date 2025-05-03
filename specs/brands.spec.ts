eb850740c2d029e7ad88a9909ea871dd47610e5b
khvalbotaihor <31129714+khvalbotaihor@users.noreply.github.com>
2025-05-03 09:53:13 +0300

    refactor

--- specs/brands.spec.ts
+++ specs/brands.spec.ts
@@ -1,278 +1,248 @@
-import { describe, it } from '@jest/globals'
-import * as supertest from 'supertest'
-import { object, number, string, array, date } from 'superstruct'
+import { describe, it } from '@jest/globals';
+import * as supertest from 'supertest';
+import { object, number, string, array, date } from 'superstruct';
 
-const request = supertest('https://practice-react.sdetunicorns.com/api/test')
+const request = supertest('https://practice-react.sdetunicorns.com/api/test');
 let brandId;
-const today = new Date()
-
+const today = new Date();
 
 const payloadForPostRequest = {
-    "name": 'test name'+ Math.floor(Math.random() *1000),
-    "description": "test description"
-}
-  
-beforeAll(async() => {
- const response = await request
-            .post('/brands')
-            .send(payloadForPostRequest)
+  name: 'test name' + Math.floor(Math.random() * 1000),
+  description: 'test description',
+};
 
- console.log('beforeAll response: ', response.body)
+beforeAll(async () => {
+  const response = await request.post('/brands').send(payloadForPostRequest);
 
- brandId = response.body['_id']
- console.log('beforeAll brandId: ', brandId)
-         
-  expect(response.status).toBe(200)
-         expect(response.body.name).toBe(payloadForPostRequest.name) 
-         expect(response.body.description).toBe(payloadForPostRequest.description)
-         expect(response.body).toHaveProperty('createdAt') 
+  console.log('beforeAll response: ', response.body);
 
-         expect(response.body.createdAt).toContain(today.getFullYear().toString())
-         expect(response.body.createdAt).toContain(today.getDate().toString().padStart(2, '0'))
+  brandId = response.body['_id'];
+  console.log('beforeAll brandId: ', brandId);
 
-  
-})
+  expect(response.status).toBe(200);
+  expect(response.body.name).toBe(payloadForPostRequest.name);
+  expect(response.body.description).toBe(payloadForPostRequest.description);
+  expect(response.body).toHaveProperty('createdAt');
+  expect(response.body.createdAt).toContain(today.getFullYear().toString());
+  expect(response.body.createdAt).toContain(today.getDate().toString().padStart(2, '0'));
+});
 
-describe('brands', ()=> {
-    it('GET brands', async () => {
-        const response = await request.get('/brands')
+describe('brands', () => {
+  it('GET brands', async () => {
+    const response = await request.get('/brands');
 
-        console.log('response', response.body)
+    console.log('response', response.body);
 
-        expect(response.status).toBe(200)
-        expect(response.body.length).toBeGreaterThan(1)
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBeGreaterThan(1);
 
-        const hasNameAndIdProperties = response.body.every(el => {
-            return '_id' in el && 'name' in el
-        })
+    const hasNameAndIdProperties = response.body.every((el) => '_id' in el && 'name' in el);
+    expect(hasNameAndIdProperties).toBeTruthy();
 
-        expect(hasNameAndIdProperties).toBeTruthy()
-        const objectKeys = Object.keys(response.body[0])
+    const objectKeys = Object.keys(response.body[0]);
+    console.log('object keys', objectKeys);
+    expect(objectKeys).toEqual(['_id', 'name']);
+  });
 
-        console.log('object keys', objectKeys)
-        expect(objectKeys).toEqual(['_id','name'])
-    })
-
-    describe('POST create a new brand', () => {
-        describe('Create brands', () => {
-            it('', async () => {
-                const response = await request
-            .post('/brands')
-            .send(payloadForPostRequest)
-
-
-      console.log('response', response.body)
-        
-         expect(response.status).toBe(200)
-         expect(response.body.name).toBe(payloadForPostRequest.name) 
-         expect(response.body.description).toBe(payloadForPostRequest.description)
-         expect(response.body).toHaveProperty('createdAt') 
-
-         expect(response.body.createdAt).toContain(today.getFullYear().toString())
-         expect(response.body.createdAt).toContain(today.getDate().toString().padStart(2, '0'))
-            })
-        })
-
-        it('Schema verification - Name is a mandatory field', async () => {
-            const response = await request
-            .post('/brands')
-            .send({
-                "name": '',
-                "description": "test description"
-              })
-
-            console.log('response', response.body)
-        
-            expect(response.status).toBe(422)
-            expect(response.body.error).toEqual('Name is required')
-        })
-
-        it('Schema verification - Name with 2 symbols validation - Valid check', async () => {
-            let a: number, b: number;
-            const value = String.fromCharCode((a = Math.floor(Math.random() * 52)) < 26 ? a + 65 : a + 71) + String.fromCharCode((b = Math.floor(Math.random() * 52)) < 26 ? b + 65 : b + 71)
-
-            
-            const payload = {
-                "name": value,
-                "description": "test description"
-              }
-            console.log('Schema verification ', payload.name)
-            const response = await request
-            .post('/brands')
-            .send(payload)
-
-            console.log('response', response.body)
-        
-            expect(response.status).toBe(200)
-            expect(response.body.name).toEqual(payload.name)
-        })
-
-        it('Schema verification - Name with 1 symbol validation - test failed', async () => {
-            const payload = {
-                "name": 't',
-                "description": "test description"
-              }
-            
-            const response = await request
-            .post('/brands')
-            .send(payload)
-
-            console.log('response', response.body)
-        
-            expect(response.status).toBe(422)
-            expect(response.body.error).toEqual('Brand name is too short')
-        })
-
-
-        it('Duplicate brand entries are not allowed', async () => {
-            const payload = {
-                "name": payloadForPostRequest.name,
-                "description": "test description"
-              }
-            
-            const response = await request
-            .post('/brands')
-            .send(payload)
-
-            console.log('response', response.body)
-        
-            expect(response.status).toBe(422)
-            expect(response.body.error).toContain(`${payload.name} already exists`)
-        })
-
-        it('Business logic - GET /brands/invalidId with throw an error', async () => {            
-            const response = await request.get('/brands/invalid_id')
-
-            console.log('response', response.body)
-        
-            expect(response.status).toBe(422)
-            expect(response.body.error).toEqual('Unable to fetch brand')
-        })
-
-        it('Business logic - GET /brands/incorrect_id with throw an error', async () => {            
-            const response = await request.get('/brands/64b8866b49e85607248e2b21')
-
-            console.log('response', response.body)
-        
-            expect(response.status).toBe(404)
-            expect(response.body.error).toEqual('Brand not found.')
-        })
-
-
-             // not working
-      it('POST Brand description must be a string', async () => {
+  describe('POST create a new brand', () => {
+    describe('Create brands', () => {
+      it('Create a new brand', async () => {
         const payload = {
-            "name": 'test name'+ Math.floor(Math.random() *1000),
-            "description": 111
-        }
+          name: 'test name' + Math.floor(Math.random() * 1000),
+          description: 'test description',}
+        const response = await request.post('/brands').send(payload);
 
-          console.log('payload', payload)
-        const response = await request.post('/brands').send(payload)
-        console.log('response', response.body)
+        console.log('response', response.body);
 
-        expect(response.status).toBe(422)
-    
-        expect(response.body.error).toBe('Brand description must be a string')
-      })
-        
+        expect(response.status).toBe(200);
+        expect(response.body.name).toEqual(payload.name);
+        expect(response.body.description).toBe(payloadForPostRequest.description);
+        expect(response.body).toHaveProperty('createdAt');
+        expect(response.body.createdAt).toContain(today.getFullYear().toString());
+        expect(response.body.createdAt).toContain(today.getDate().toString().padStart(2, '0'));
+      });
+    });
 
-    })
+    it('Schema verification - Name is a mandatory field', async () => {
+      const response = await request.post('/brands').send({
+        name: '',
+        description: 'test description',
+      });
 
-    it('GET brand/:id', async () => {
-        const response = await request.get('/brands/'+ brandId)
-        console.log('response', response.body)
+      console.log('response', response.body);
 
-        expect(response.status).toBe(200)
-        expect(response.body.name).toBe(payloadForPostRequest.name)
-    })
+      expect(response.status).toBe(422);
+      expect(response.body.error).toEqual('Name is required');
+    });
 
-    describe('PUT update brand information', () => {
+    it('Schema verification - Name with 2 symbols validation - Valid check', async () => {
+      const value =
+        String.fromCharCode(Math.random() * 26 + 65) +
+        String.fromCharCode(Math.random() * 26 + 65);
 
-      it('PUT', async () => {
-        const payload = {
-            "name": 'new name'+ Math.floor(Math.random() *1000)        
-          }
+      const payload = {
+        name: value,
+        description: 'test description',
+      };
 
-          console.log('brandId in PUT request', brandId)
-          console.log('payload', payload)
-        const response = await request.put(`/brands/${brandId}`).send(payload)
+      console.log('Schema verification ', payload.name);
+      const response = await request.post('/brands').send(payload);
 
-        expect(response.status).toBe(200)
-        console.log('response', response.body)
-    
-        expect(response.body.name).toBe(payload.name)
-      })
+      console.log('response', response.body);
 
-      it('PUT check name validation on 30 symbols are allowed', async () => {
-        const randomText: string = Array.from({ length: 30 }, () => {
-            const r: number = Math.floor(Math.random() * 52);
-            return String.fromCharCode(r < 26 ? r + 65 : r + 71);
-          }).join('');
+      expect(response.status).toBe(200);
+      expect(response.body.name).toEqual(payload.name);
+    });
 
-        const payload = {
-            "name": randomText       
-          }
+    it('Schema verification - Name with 1 symbol validation - test failed', async () => {
+      const payload = {
+        name: 't',
+        description: 'test description',
+      };
 
-          console.log('brandId in PUT request', brandId)
-          console.log('payload', payload)
-        const response = await request.put(`/brands/${brandId}`).send(payload)
+      const response = await request.post('/brands').send(payload);
 
+      console.log('response', response.body);
 
-        expect(response.status).toBe(200)
-        console.log('response', response.body)
-    
-        expect(response.body.name).toBe(payload.name)
-      })
+      expect(response.status).toBe(422);
+      expect(response.body.error).toEqual('Brand name is too short');
+    });
 
-      it('PUT check name validation on 31 symbols are allowed', async () => {
-        const payload = {
-            "name": 'this is thirtyO characters long'       
-          }
+    it('Duplicate brand entries are not allowed', async () => {
+      const payload = {
+        name: payloadForPostRequest.name,
+        description: 'test description',
+      };
 
-          console.log('payload', payload)
-        const response = await request.put(`/brands/${brandId}`).send(payload)
-        console.log('response', response.body)
+      const response = await request.post('/brands').send(payload);
 
-        expect(response.status).toBe(422)
-    
-        expect(response.body.error).toBe('Brand name is too long')
-      })
+      console.log('response', response.body);
 
+      expect(response.status).toBe(422);
+      expect(response.body.error).toContain(`${payload.name} already exists`);
+    });
 
+    it('Business logic - GET /brands/invalidId will throw an error', async () => {
+      const response = await request.get('/brands/invalid_id');
 
-      it('PUT An error is shown when updating invalid brand', async () => {
-        const payload = {
-            "name":'test name'+ Math.floor(Math.random() *1000) ,
-            describe: 'test describe'     
-          }
+      console.log('response', response.body);
 
-          console.log('payload', payload)
-        const response = await request.put('/brands/64b8853449e85607248e2b11').send(payload)
-        console.log('response', response.body)
+      expect(response.status).toBe(422);
+      expect(response.body.error).toEqual('Unable to fetch brand');
+    });
 
-        expect(response.status).toBe(404)
-    
-        expect(response.body.error).toBe('Brand not found.')
-      })
-    })
+    it('Business logic - GET /brands/incorrect_id will throw an error', async () => {
+      const response = await request.get('/brands/64b8866b49e85607248e2b21');
 
-    describe('DELETE brand',  () => {
+      console.log('response', response.body);
 
-        it('DELETE', async () => {
-            const response = await request.delete(`/brands/${brandId}`)
+      expect(response.status).toBe(404);
+      expect(response.body.error).toEqual('Brand not found.');
+    });
 
-            expect(response.status).toEqual(200)
-            expect(response.body).toEqual(null)
-        })
+    it('POST Brand description must be a string', async () => {
+      const payload = {
+        name: 'test name' + Math.floor(Math.random() * 1000),
+        description: 111,
+      };
 
-        it('DELETE invalid brand by invalid brandId', async () => {
-            const response = await request.delete('/brands/64b8853449e85607248e2b11')
+      console.log('payload', payload);
+      const response = await request.post('/brands').send(payload);
 
-            expect(response.status).toEqual(404)
-            expect(response.body.error).toEqual('Brand not found.')
-        })
+      console.log('response', response.body);
 
+      expect(response.status).toBe(422);
+      expect(response.body.error).toBe('Brand description must be a string');
+    });
+  });
 
-       
-    })
-})
+  it('GET brand/:id', async () => {
+    const response = await request.get('/brands/' + brandId);
+
+    console.log('response', response.body);
+
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe(payloadForPostRequest.name);
+  });
+
+  describe('PUT update brand information', () => {
+    it('PUT', async () => {
+      const payload = {
+        name: 'new name' + Math.floor(Math.random() * 1000),
+      };
+
+      console.log('brandId in PUT request', brandId);
+      console.log('payload', payload);
+      const response = await request.put(`/brands/${brandId}`).send(payload);
+
+      expect(response.status).toBe(200);
+      console.log('response', response.body);
+
+      expect(response.body.name).toBe(payload.name);
+    });
+
+    it('PUT check name validation on 30 symbols are allowed', async () => {
+      const randomText = Array.from({ length: 30 }, () =>
+        String.fromCharCode(Math.random() * 26 + 65)
+      ).join('');
+
+      const payload = {
+        name: randomText,
+      };
+
+      console.log('brandId in PUT request', brandId);
+      console.log('payload', payload);
+      const response = await request.put(`/brands/${brandId}`).send(payload);
+
+      expect(response.status).toBe(200);
+      console.log('response', response.body);
+
+      expect(response.body.name).toBe(payload.name);
+    });
+
+    it('PUT check name validation on 31 symbols are not allowed', async () => {
+      const payload = {
+        name: 'this is thirtyO characters long',
+      };
+
+      console.log('payload', payload);
+      const response = await request.put(`/brands/${brandId}`).send(payload);
+
+      console.log('response', response.body);
+
+      expect(response.status).toBe(422);
+      expect(response.body.error).toBe('Brand name is too long');
+    });
+
+    it('PUT An error is shown when updating invalid brand', async () => {
+      const payload = {
+        name: 'test name' + Math.floor(Math.random() * 1000),
+        describe: 'test describe',
+      };
+
+      console.log('payload', payload);
+      const response = await request.put('/brands/64b8853449e85607248e2b11').send(payload);
+
+      console.log('response', response.body);
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBe('Brand not found.');
+    });
+  });
+
+  describe('DELETE brand', () => {
+    it('DELETE', async () => {
+      const response = await request.delete(`/brands/${brandId}`);
+
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual(null);
+    });
+
+    it('DELETE invalid brand by invalid brandId', async () => {
+      const response = await request.delete('/brands/64b8853449e85607248e2b11');
+
+      expect(response.status).toEqual(404);
+      expect(response.body.error).toEqual('Brand not found.');
+    });
+  });
+});
